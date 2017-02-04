a304436b6f523a6580825b438524e8c85fe76b1d
jiangyl <jiagnyl@gclasscn.com>
2017-02-04 17:08:00 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -9,29 +9,33 @@ $(function(){
 			image.src=url;//将input得到的url赋值给新建的图片对象
 
 			//选中图像居中
+			$(".box_le_main").css({"width":"100%","height":"100%","top":"0","left":"0"});
 			if(image.width<image.height){
-				$(".j-img-back").addClass("my_img_hei").removeClass("my_img_wid");
-				$(".j-img-ago").addClass("my_img_hei").removeClass("my_img_wid");
+				$(".box_le_img").addClass("my_img_hei").removeClass("my_img_wid");
 			}else{
-				$(".j-img-back").addClass("my_img_wid").removeClass("my_img_hei");
-				$(".j-img-ago").addClass("my_img_wid").removeClass("my_img_hei");
+				$(".box_le_img").addClass("my_img_wid").removeClass("my_img_hei");
 			}
-			$(".j-img-back").attr("src",image.src);
-			$(".j-img-ago").attr("src",image.src);
-			$(".box_le_main").width($(".j-img-back").width()).height($(".j-img-back").height()).css({"top":($(".box_ri_main").height()-$(".j-img-back").height())/2+"px","left":($(".box_ri_main").width()-$(".j-img-back").width())/2+"px"});
+			$(".box_le_img").attr("src",image.src);
+			$(".box_le_main").width($(".j-img-back").width()).height($(".j-img-back").height())
+				.css({"top":($(".box_le_ma").height()-$(".box_le_main").height())/2+"px","left":($(".box_le_ma").width()-$(".box_le_main").width())/2+"px"});
 			$(".box_le_ma").css({"border-radius":"0px"});
 
+
+			ThisWidth=$(".box_le_mb").width();//截取区域width
+			ThisHeight=$(".box_le_mb").height();//截取区域height
 			ParentTop=$(".box_le_main").offset().top;//移动区域所在的top位置
 			ParentLeft=$(".box_le_main").offset().left;//移动区域所在的left位置
-			ParenWidth=$(".box_le_main").width()-$(".box_le_mb").width();//移动区域所在的宽度
-			ParenHeight=$(".box_le_main").height()-$(".box_le_mb").height();//移动区域所在的高度
+			ParenWidth=$(".box_le_main").width()-ThisWidth;//移动区域所在的宽度
+			ParenHeight=$(".box_le_main").height()-ThisHeight;//移动区域所在的高度
 		}
 	})
 	
 	//声明拖动区域
 
-	var ThisMou=false;//当前是否按下状态
-	var ThisHover=false;//当前鼠标是否在元素上状态
+	var ThisConMou=false;//当前是否在选中区按下状态
+	var ThisConHover=false;//当前鼠标是否在选中区元素上状态
+	var ThisSizeMou=false;//当前是否在调整大小区按下状态
+	var ThisSizeHover=false;//当前是否在调整大小区素上状态
 	var ClaRangeTop=0;//鼠标在元素中的top位置
 	var ClaRangeLeft=0;//鼠标在元素中的left位置
 	var ParentTop;//移动区域所在的top位置
@@ -40,18 +44,21 @@ $(function(){
 	var ParenHeight;//移动区域所在的高度
 	var ThisTop;//截取区域top
 	var ThisLeft;//截取区域left
+	var ThisWidth=$(".box_le_mb").width();//截取区域width
+	var ThisHeight=$(".box_le_mb").height();//截取区域height
 	$(".box_le_mb").hover(function(){
-		ThisHover=true;
+		ThisConHover=true;
 	},function(){
-		ThisHover=false;
+		ThisConHover=false;
 	})
 	$(".box_le_mb").mousedown(function(ev){
-		ThisMou=true;
+		ThisConMou=true;
+		//鼠标在选中区中的位置
 		ClaRangeTop=ev.pageY-$(this).offset().top;
 		ClaRangeLeft=ev.pageX-$(this).offset().left;
 	})
 	$(".box_le_mb").mouseup(function(){
-		ThisMou=false;
+		ThisConMou=false;
 
 		//截取图片
 		var canvas=document.getElementById("canvas");
@@ -62,19 +69,50 @@ $(function(){
 		//取得截取坐标
 		var x=ThisLeft/$(".j-img-back").width()*theImage.width;
 		var y=ThisTop/$(".j-img-back").height()*theImage.height;
-		var thewidth=theImage.width/thisimg.width*50;
-		var theheight=theImage.height/thisimg.height*50;
+		var thewidth=theImage.width/thisimg.width*ThisWidth;
+		var theheight=theImage.height/thisimg.height*ThisHeight;
+		console.log(ThisWidth)
 		ctx.drawImage(thisimg,x,y,thewidth,theheight, 0,0,180,180);
 
 	})
+	//截取区域拖动效果
 	$(".box_le_mb").mousemove(function(ev){
-		if(ThisMou==true){
+		console.log(ThisConMou,ThisConHover)
+		if(ThisConMou==true && ThisConHover==true){
 			ThisTop=ev.pageY-ParentTop-ClaRangeTop;
 			ThisLeft=ev.pageX-ParentLeft-ClaRangeLeft;
 			if(ThisTop>=0 && ThisTop<=ParenHeight && ThisLeft>=0 && ThisLeft<=ParenWidth){
 				$(this).css("transform","translate("+ThisLeft+"px,"+ThisTop+"px)");
-				$(".j-img-ago").css("clip","rect("+ThisTop+"px,"+(ThisLeft+50)+"px,"+(ThisTop+50)+"px,"+ThisLeft+"px)");
+				$(".j-img-ago").css("clip","rect("+ThisTop+"px,"+(ThisLeft+ThisWidth)+"px,"+(ThisTop+ThisHeight)+"px,"+ThisLeft+"px)");
 			}
 		}
+	})
+	$(".box_le_mb_co").mousedown(function () {
+		ThisConHover=false;
+		ThisSizeMou=true;
+	})
+	$(".box_le_mb_co").mouseup(function () {
+		ThisConHover=true;
+		ThisSizeMou=false;
+	})
+	$(".box_le_mb_co").hover(function () {
+		ThisSizeHover=true;
+	},function () {
+		ThisSizeHover=false;
+	})
+	$(".box_le_mb_co").mousemove(function(ev){
+		if(ThisSizeMou==true && ThisSizeHover==true){
+			$(".box_le_mb").width(ev.pageX-ParentLeft).height(ev.pageY-ParentTop);
+			console.log("d"+ev.pageX+"n"+ParentLeft)
+		}
+	})
+	$(".box_le_ma").mouseup(function () {
+		ThisConMou=false;
+		// ThisConHover=false;
+		ThisSizeMou=false;
+		// ThisSizeHover=false;
+	})
+	$(".box_le_ma").mousedown(function () {
+		console.log(ThisConMou,ThisConHover,ThisSizeMou,ThisSizeHover)
 	})
 })
